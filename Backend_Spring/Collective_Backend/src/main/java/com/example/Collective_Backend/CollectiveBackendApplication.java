6f8a995a9b6591129d903059cd98befbe504baea
DragonZ <drag.popescu@yahoo.com>
2022-11-08 16:25:33 +0200

    Added Spring Project

---
+++ Backend_Spring/Collective_Backend/src/main/java/com/example/Collective_Backend/CollectiveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Collective_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectiveBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectiveBackendApplication.class, args);
+	}
+
+}
---
+++ Backend_Spring/Collective_Backend/src/test/java/com/example/Collective_Backend/CollectiveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Collective_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectiveBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
