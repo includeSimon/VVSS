e4d95350e2047118219d27794cbca626cbfa715d
Andrei Balu <78239922+Baloo31@users.noreply.github.com>
2022-11-13 14:51:49 +0200

    created backend module, backend service, user module, user-register-form

---
+++ Frontend_Angular/Collective_Frontend/src/app/user/user.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserRegisterFormComponent } from './user-register-form/user-register-form.component';
+
+
+
+@NgModule({
+  declarations: [
+    UserRegisterFormComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UserModule { }
