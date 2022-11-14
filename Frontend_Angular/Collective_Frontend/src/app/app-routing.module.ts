8ae47b02f32dbc8297ce285992cc4f917502c411
Andrei Balu <78239922+Baloo31@users.noreply.github.com>
2022-11-14 22:43:37 +0200

    user-register-form component design and logic, basic validators

--- Frontend_Angular/Collective_Frontend/src/app/app-routing.module.ts
+++ Frontend_Angular/Collective_Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {UserRegisterFormComponent} from "./user/user-register-form/user-register-form.component";
+import {AppComponent} from "./app.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'register',
+    component: UserRegisterFormComponent,
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
