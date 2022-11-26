3c5ca922f6b298585f3ef59ad99879410abbff29
Andrei Balu <78239922+Baloo31@users.noreply.github.com>
2022-11-26 22:21:26 +0200

    assign and unassign task features frontend

--- Frontend_Angular/Collective_Frontend/src/app/task/task.module.ts
+++ Frontend_Angular/Collective_Frontend/src/app/task/task.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {AssignUnassignTaskComponent} from "./assign-unassign-task/assign-unassign-task.component";
 import {MatTableModule} from "@angular/material/table";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatSelectModule} from "@angular/material/select";
+import {FormsModule} from "@angular/forms";
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
 
 
 
@@ -14,7 +19,12 @@ import {MatTableModule} from "@angular/material/table";
   ],
   imports: [
     CommonModule,
-    MatTableModule
+    MatTableModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    FormsModule,
+    MatCardModule,
+    MatButtonModule
   ]
 })
 export class TaskModule { }
