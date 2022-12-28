c641e8dbaae0bc40079d7eb55b31e88bb46e4d90
morar0108 <maria.morar2001@yahoo.com>
2022-12-28 21:44:56 +0200

    beginning sidenav

---
+++ Frontend_Angular/Collective_Frontend/src/app/sidenav/sidenav.module.ts
@@ -0,0 +1,31 @@
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { SidenavComponent } from './components/sidenav/sidenav.component';
+// import {BrowserModule} from "@angular/platform-browser";
+// import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+// import {MatButtonModule} from "@angular/material/button";
+// import {MatDividerModule} from "@angular/material/divider";
+// import {MatIconModule} from "@angular/material/icon";
+// import {MatSidenavModule} from "@angular/material/sidenav";
+// import {MatToolbarModule} from "@angular/material/toolbar";
+// import {AppRoutingModule} from "../app-routing.module";
+//
+//
+//
+// @NgModule({
+//   declarations: [
+//     SidenavComponent
+//   ],
+//   imports: [
+//     CommonModule,
+//     BrowserModule,
+//     AppRoutingModule,
+//     BrowserAnimationsModule,
+//     MatToolbarModule,
+//     MatSidenavModule,
+//     MatButtonModule,
+//     MatIconModule,
+//     MatDividerModule
+//   ]
+// })
+// export class SidenavModule { }
